d2d8643b2bd3c8739e5dd34c2c9237c6fb814563
lnsohu <35254544+lnsohu@users.noreply.github.com>
2025-02-17 11:16:12 +0800

    Delete routes/api.js

--- routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// 返回 "Hello World" 的 API 接口
-router.get('/hello', (req, res) => {
-  res.json({ message: 'Hello World from LeanCloud Backend!' });
-});
-
-module.exports = router;
