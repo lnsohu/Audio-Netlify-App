ae22b54b3a7372fd9b2918137a3b216b72317d3d
lnsohu <35254544+lnsohu@users.noreply.github.com>
2025-05-07 21:23:32 +0800

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// 定义根路由，返回 "Hello World"
-router.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-module.exports = router;
