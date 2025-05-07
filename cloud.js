a2120ed008a9b37dee5db6d1b251e649c7cac2f2
lnsohu <35254544+lnsohu@users.noreply.github.com>
2025-05-07 21:09:56 +0800

    Update and rename cloud.js to netlify.toml

--- cloud.js
+++
@@ -1,9 +0,0 @@
-const leanengine = require('leanengine');
-
-leanengine.init({
-  appId: process.env.LEANCLOUD_APP_ID,
-  appKey: process.env.LEANCLOUD_APP_KEY,
-  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
-});
-
-const app = require('./app');
